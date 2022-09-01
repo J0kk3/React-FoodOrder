4df767ae87eba80aec45997f97d0c4bbd15024cd
J0kk3 <joakim_88@msn.com>
2022-09-01 23:14:57 +0200

    [ADD] cart - done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+//hooks
 import { useState } from 'react';
 //components
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import Cart from './components/Cart/Cart';
+import CartProvider from './store/CartProvider';
 
 function App ()
 {
@@ -19,13 +21,13 @@ function App ()
   };
 
   return (
-    <>
+    <CartProvider>
       { cartIsShown && <Cart onClose={ hideCartHandler } /> }
       <Header onShowCart={ showCartHandler } />
       <main>
         <Meals />
       </main>
-    </>
+    </CartProvider>
   );
 }
 
--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,17 +1,13 @@
-import React from 'react';
-import { ReactDOM } from 'react';
+import ReactDOM from 'react-dom';
 //styles
 import classes from './Modal.module.css';
 
-const Backdrop = props =>
+const Backdrop = ( props ) =>
 {
-    return (
-        <div className={ classes.backdrop } onClick={ props.onClose }>
-        </div>
-    );
+    return <div className={ classes.backdrop } onClick={ props.onClose } />;
 };
 
-const ModalOverlay = props =>
+const ModalOverlay = ( props ) =>
 {
     return (
         <div className={ classes.modal }>
@@ -22,12 +18,15 @@ const ModalOverlay = props =>
 
 const portalElement = document.getElementById( 'overlays' );
 
-const Modal = props =>
+const Modal = ( props ) =>
 {
     return (
         <>
             { ReactDOM.createPortal( <Backdrop onClose={ props.onClose } />, portalElement ) }
-            { ReactDOM.createPortal( <ModalOverlay>{ props.children }</ModalOverlay>, portalElement ) }
+            { ReactDOM.createPortal(
+                <ModalOverlay>{ props.children }</ModalOverlay>,
+                portalElement
+            ) }
         </>
     );
 };
